ac77edbe6e30d5a29a48f9759512d5fb6e00dd57
tawwn <124248186+tawwn@users.noreply.github.com>
2025-04-03 00:54:21 +0700

    n

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,17 +1,31 @@
 using System.Net;
 using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Entity Framework to use SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer("Server=localhost;Database=Data;User Id=SA;Password=St0rngP@ss123!;"
-));
+    options.UseSqlServer("Server=localhost;Database=Data;User Id=SA;Password=St0rngP@ss123!;")
+);
 
 // Configure Kestrel server to listen on all IPs on port 5000
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
     serverOptions.Listen(IPAddress.Any, 5000);
+});
+
+// Add CORS support
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
 });
 
 var app = builder.Build();
@@ -23,12 +37,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 
-// API Endpoint to get Name and Email
+// API Endpoint to get username and email
 app.MapGet("/users", async (ApplicationDbContext dbContext) =>
 {
     var users = await dbContext.Users
-        .Select(u => new { u.Name, u.Email })
+        .Select(u => new { u.username, u.email })
         .ToListAsync();
 
     return users;
--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab1b0f7e81b35627f6a0c7510d7b59fd5e6337d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f39fb53d40d622283252bc6b9ed8b484107040")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
